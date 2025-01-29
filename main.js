21b62e70ffa381d42c10fcccf205ca0f3b76d15c
Luis Barbara <luisbarbara@live.com.pt>
2025-01-29 21:54:09 +0000

    build: main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+let horas = prompt("Que horas são?")
+
+
+
+if (horas < 13) {
+    console.log("Bom dia");
+}
+else if (horas >= 13 && horas < 19) {
+    console.log("Boa tarde");
+}
+else {
+    console.log("Boa noite");
+}
+
+
