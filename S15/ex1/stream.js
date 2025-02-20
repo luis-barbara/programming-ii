7a3859a88233eb58e90027c3757acb12eb348f53
Luís Bárbara <150970169+luis-barbara@users.noreply.github.com>
2025-02-20 21:47:04 +0000

    Add files via upload

---
+++ S15/ex1/stream.js
@@ -0,0 +1,18 @@
+// stream.js
+import { createReadStream } from 'fs';  
+const stream = createReadStream('users.csv', 'utf-8');  
+stream.on('data', chunk => console.log('Chunk:', chunk.length));  
+
+
+
+export class Transform{
+    #filePath;
+    constructor(filePath){
+        this.#filePath = filePath;
+    }
+
+    start(){
+        const stream = createReadStream(this.#filePath, "utf-8");  //(this.#filePath{encoding:"utf-8", highWaterMark: 100}); - separa os ficheiros em diferentes com 100 caracteres/bytes
+        stream.on('data', chunk => console.log(`Chunk: ${chunk.length}`));  
+    }
+};
